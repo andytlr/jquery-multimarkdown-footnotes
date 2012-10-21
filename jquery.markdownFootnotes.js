0985c253a3d8f25e6ab79da07ae088f7f9072099
Andy Taylor <andytlr@gmail.com>
2012-10-21 23:21:58 +1100

    Minor styling changes.
    Will attempt to re-style to make more usable on small screens.

--- jquery.markdownFootnotes.js
+++ jquery.markdownFootnotes.js
@@ -2,10 +2,7 @@ $(function() {
 	// To edit the display of the footnote
 	var footnoteContentCSS = {
 		'background': '#000',
-		'opacity': '.9',
-		'color': '#fff',
-		'-webkit-box-shadow': '0px 0px 5px #000',
-		'-moz-box-shadow': '0px 0px 5px #000'
+		'color': '#fff'
 	}
 	
 	// To edit the display of the link that closes the footnote
@@ -23,7 +20,7 @@ $(function() {
 		var token = link.attr('href').substr(1);
 		var footnoteContent = $(document.getElementById(token)).html();
 		
-		$('body').append('<div id="overlay-' + token + '" class="footnoteContent" style="display: none; position: fixed; bottom: 0; left: 50%; padding: 2%; width: 80%; margin-left: -42%;">' + footnoteContent + '</div>');
+		$('body').append('<div id="overlay-' + token + '" class="footnoteContent" style="display: none; position: fixed; bottom: 0; left: 0;">' + footnoteContent + '</div>');
 		
 		link.click(function(){
 			var $currentFootnote = $(document.getElementById('overlay-' + token));
